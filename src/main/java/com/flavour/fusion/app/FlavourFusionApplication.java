6bcae7a2502cef34483b3d8e14d516d68f371fcf
Parv Nautiyal <105883196+parvnautiyal@users.noreply.github.com>
2023-11-07 02:12:45 +0530

    initial commit

---
+++ src/main/java/com/flavour/fusion/app/FlavourFusionApplication.java
@@ -0,0 +1,13 @@
+package com.flavour.fusion.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavourFusionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlavourFusionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flavour/fusion/app/FlavourFusionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flavour.fusion.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlavourFusionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
